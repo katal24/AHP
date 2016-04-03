2e9dedb2a93354d2cc48f2449a5230bc3d1561a5
dawid <talagadawid@gmail.com>
2016-04-04 00:14:16 +0200

    wszystko

---
+++ src/main/java/sp/BaseController.java
@@ -0,0 +1,49 @@
+package sp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by dawid on 02.04.16.
+ */
+//
+
+//@RequestMapping("/kontroler")
+//@EnableAutoConfiguration
+@Controller
+public class BaseController {
+
+    @RequestMapping("/hello")
+    public String przykladModelu(Model model) {
+        model.addAttribute("message", "To jest super inormacja przekazana z widoku");
+        return "hello";
+    }
+
+    @RequestMapping("/witaj")
+    public String przyklad(Model model) {
+        model.addAttribute("powitanie", "Mile powitanie? NIEEEEEEE");
+        return "witaj";
+    }
+
+//    @Autowired
+//    private Inquiry inquiry;        // chcemy miec obiekt Inquiry i miec go dostepnego pod nazwa inquiry
+//
+//    @RequestMapping("/metoda")
+//    public static String metoda(){
+//        return "glowny";
+//    }
+//
+//    // metoda ustawia atrybut, ktory bedzie dostepny z poziomu widoku:  ${message}
+//    @RequestMapping("/przyklad/model")
+//    public String przykladModelu(Model model){
+//        model.addAttribute("message", "To jest super inormacja przekazana z widoku");
+//        return "glowny";
+//    }
+
+//    @RequestMapping("/main")
+//    public static void main(String[] args){
+//        //SpringApplication.run(BaseController.class, args);
+//     //   metoda();
+//    }
+}
