31024c69ca9c87057fd5c6839a34fe0b58333aac
fijalkovsky <lilka@windowslive.com>
2016-04-28 13:53:03 +0200

    createSurvey modification :D 2

--- web/resources/js/main.js
+++ web/resources/js/main.js
@@ -85,16 +85,13 @@ myApp.controller('loginController', function($rootScope, $scope, $http, $locatio
 myApp.controller('createSurveyController', function($scope) {
     $scope.message = 'Create survey';
 
-    $('.multi-field-wrapper').each(function() {
-        var $wrapper = $('.multi-fields', this);
-        $(".add-field", $(this)).click(function(e) {
-            $('.multi-field:first-child', $wrapper).clone(true).appendTo($wrapper).find('input').val('').focus();
-        });
-        $('.multi-field .remove-field', $wrapper).click(function() {
-            if ($('.multi-field', $wrapper).length > 1)
-                $(this).parent('.multi-field').remove();
-        });
-    });
+    $scope.getSurveydatas = function () {
+
+        var surveyName = document.getElementById('surveyName').value;
+        surveyName.innerHTML;
+
+        
+    }
 
 });
 myApp.controller('newAccountController', function($scope) {
