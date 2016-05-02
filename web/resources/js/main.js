1ea14d4acfa02a41df6b8a2804960692d3d7c0c7
fijalkovsky <lilka@windowslive.com>
2016-05-02 20:26:42 +0200

    dziala!!!

--- web/resources/js/main.js
+++ web/resources/js/main.js
@@ -85,10 +85,30 @@ myApp.controller('loginController', function($rootScope, $scope, $http, $locatio
 myApp.controller('createSurveyController', function($scope) {
     $scope.message = 'Create survey';
 
+    $scope.choices = [{id: 'choice1'}, {id: 'choice2'}];
+
+    $scope.addNewChoice = function() {
+        var newItemNo = $scope.choices.length+1;
+        $scope.choices.push({'id':'choice'+newItemNo});
+    };
+
+    $scope.removeChoice = function() {
+        var lastItem = $scope.choices.length-1;
+        $scope.choices.splice(lastItem);
+    };
+
+
     $scope.getSurveydatas = function () {
 
-        var surveyName = document.getElementById('surveyName').value;
-        $scope.surveyName = surveyName;
+        var ep = {
+            surveyName: $scope.surveyName,
+            categroy1: $scope.edProf.age,
+            edprofheight: $scope.edProf.height,
+            edprofweight: $scope.edProf.weight,
+            edprofactivity: $scope.edProf.activity,
+            edprofvegetarian: $scope.edProf.vegetarian
+
+        };
 
         
     }
